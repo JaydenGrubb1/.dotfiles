1903a68e9bf4e363ee9ac5d89b2b0657c11d069c
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-13 15:20:45 +1000

    Add whitespace highlighting to nvim config

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,4 +6,15 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
+vim.opt.wrap = false
+
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = "▸ ",
+	leadmultispace = "·",
+	trail = "·",
+	extends = "»",
+	precedes = "«",
+	nbsp = "␣",
+}
