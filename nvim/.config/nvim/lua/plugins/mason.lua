7d1c70a2f04de21970084475ce9a1e50d6599d45
Jayden Grubb <jaydengrubb1@gmail.com>
2025-03-25 17:27:33 +1000

    Add code completions to nvim config with blink.cmp

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -22,10 +22,16 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "<C-u>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
