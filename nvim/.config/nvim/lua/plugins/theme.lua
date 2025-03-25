ed53ea49350e71ccc31f1f8febc62e45d7bcd9d0
Jayden Grubb <jaydengrubb1@gmail.com>
2025-03-25 17:30:25 +1000

    Remove old catppuccin theme config

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,21 +1,3 @@
---[[
-return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNCFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
-	end,
-}
-]]
---
-
 return {
 	"Mofiqul/vscode.nvim",
 	name = "vscode",
@@ -24,5 +6,6 @@ return {
 		vim.cmd.colorscheme("vscode")
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NeoTreeDimText", { bg = "none" })
 	end,
 }
