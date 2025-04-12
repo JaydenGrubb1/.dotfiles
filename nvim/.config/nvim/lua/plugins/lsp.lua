b53e8acf1b46ad7105135b847f697d7d4b8bc87c
Jayden Grubb <jaydengrubb1@gmail.com>
2025-04-12 15:15:07 +1000

    Renamed mason to lsp and disabled lsp highlights for c++ operators
    - renamed mason.lua to lsp.lua to describe its functionality better
    - disabled lsp highlights for c++ operators because the lsp labels
      new/delete as operators (technically true) rather than keywords

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -33,9 +33,17 @@ return {
 				capabilities = capabilities,
 			})
 
-			vim.keymap.set("n", "<C-u>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "cpp",
+				callback = function()
+					-- Disable LSP highlighting for C++ operators
+					-- Allows TreeSitter to highlight them instead
+					vim.api.nvim_set_hl(0, "@lsp.type.operator.cpp", {})
+				end,
+			})
 		end,
 	},
 }
